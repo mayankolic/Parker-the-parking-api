a8bd002689c2785e455f9c6fa246a8183e06de6e
Mayank <jhamayank96@gmail.com>
2022-03-29 20:28:51 +0530

    done

--- index.js
+++ index.js
@@ -7,74 +7,82 @@ const DispatchedVehicles = require('./DispatchedVehicles');
 const port = process.env.PORT || 3000;
 const CONNECTION_URL = "mongodb://parker-010:parker-010@cluster0-shard-00-00.qrrnc.mongodb.net:27017,cluster0-shard-00-01.qrrnc.mongodb.net:27017,cluster0-shard-00-02.qrrnc.mongodb.net:27017/ParkingData?ssl=true&replicaSet=atlas-gl57w1-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(port, () => console.log(`Server Running on Port: http://localhost:${port}`)))
-    .catch((error) => console.log(`${error} did not connect`));
+  .then(() => app.listen(port, () => console.log(`Server Running on Port: http://localhost:${port}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
 
-app.post("/", async (req, res) => {
-    //this will add entry to the parkedvehicles
-    const ownerName = req.params.ownerName;
-    const vehicleType = req.params.vehicleType;
-    const vehicleNumber = req.params.vehicleNumber;
-    const entryTime = req.params.entryTime;
-    try {
-        const newParkedVehicle = await ParkedVehicles.create({
-            ownerName,
-            vehicleType,
-            vehicleNumber,
-            entryTime
-        });
-        res.json(newParkedVehicle);
-    } catch (e) {
-        res.status(500).send(e);
-    }
+app.post("/", async(req, res) => {
+  //this will add entry to the parkedvehicles
+  const ownerName = req.params.ownerName;
+  const vehicleType = req.params.vehicleType;
+  const vehicleNumber = req.params.vehicleNumber;
+  const entryTime = req.params.entryTime;
+  try {
+    const newParkedVehicle = await ParkedVehicles.create({
+      ownerName,
+      vehicleType,
+      vehicleNumber,
+      entryTime
+    });
+    res.json(newParkedVehicle);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
-app.get('/', async (req, res) => {
-    try {
-        const vehicles = await ParkedVehicles.find();
-        res.json(vehicles);
-    } catch (e) {
-        console.log(e);
-    }
-});
-
-app.get('/home', async (req, res) => {
-    try {
-        const parkedVaahan = await ParkedVehicles.find();
-        const dispatchedVaahan = await DispatchedVehicles.find();
-        res.json({ status: 'ok', ParkedVehiclesLength: parkedVaahan.length, DispatchedVehicles: dispatchedVaahan.length });
-    } catch (e) {
-        console.log(e);
-    }
+app.get('/', async(req, res) => {
+  try {
+    const vehicles = await ParkedVehicles.find();
+    res.json(vehicles);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
+app.get('/home', async(req, res) => {
+  try {
+    const parkedVaahan = await ParkedVehicles.find();
+    const dispatchedVaahan = await DispatchedVehicles.find();
+    res.json({ status: 'ok', ParkedVehiclesLength: parkedVaahan.length, DispatchedVehicles: dispatchedVaahan.length });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-app.get('/parkedVehicles', async (req, res) => {
-    try {
-        const parkedVaahan = await ParkedVehicles.find();
-        res.json({ status: "ok", data: parkedVaahan });
-    } catch (e) {
-        throw new Error("Gaand fatt gayi behnchod");
-    }
+
+app.get('/parkedVehicles', async(req, res) => {
+  try {
+    const parkedVaahan = await ParkedVehicles.find();
+    res.json({ status: "ok", data: parkedVaahan });
+  } catch (e) {
+    throw new Error("Gaand fatt gayi behnchod");
+  }
 })
-app.get('/dispatchedvehicles', async (req, res) => {
-    try {
-        const dispatchedVaahan = await DispatchedVehicles.find();
-        res.json({ status: "ok", data: dispatchedVaahan });
-    }
-    catch (e) {
-        throw new Error("Le bete ja Maa chuda!!")
-    }
+app.get('/dispatchedvehicles', async(req, res) => {
+  try {
+    const dispatchedVaahan = await DispatchedVehicles.find();
+    res.json({ status: "ok", data: dispatchedVaahan });
+  } catch (e) {
+    throw new Error("Le bete ja Maa chuda!!")
+  }
 });
-app.delete('/:vehicleNumber', async (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    try {
-        const vehicletobedischarged = await ParkedVehicles.findBy({ vehicleNumber: id });
-        await vehicletobedischarged.delete();
-        res.json({ status: "ok", data: ParkedVehicles, by_id: vehicletobedischarged });
-    }
-    catch (err) {
-        throw new error("KUch gadbad hai!!");
-    }
+app.get('/:vehicleNumber', async(req, res) => {
+
+  const number = req.params.vehicleNumber;
+  try {
+    var data = await ParkedVehicles.findOne({ vehicleNumber: number });
+    console.log((data));
+    var currt = new Date();
+    res.json({ ownerName: data.ownerName, vehicleType: data.vehicleType, vehicleNumber: data.vehicleNumber, EntryTime: data.createdAt, DispatchedTime: currt.toISOString() });
+    // var vehicletobed
+
+  } catch (e) { console.log(e); }
+  // const { id } = req.params;
+  // console.log(id);
+  // try {
+  //   const vehicletobedischarged = await ParkedVehicles.findBy({ vehicleNumber: id });
+  //   await vehicletobedischarged.delete();
+  //   res.json({ status: "ok", data: ParkedVehicles, by_id: vehicletobedischarged });
+  // } catch (err) {
+  //   throw new error("KUch gadbad hai!!");
+  // }
 })
